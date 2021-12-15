c80704c198d5a5426bee95123438890ec419ad17
Shahana M <39371786+shahana-45@users.noreply.github.com>
2021-12-15 16:33:38 +0100

    Update Exercise_sheet_7.r

--- Exercise_sheet_7.r
+++ Exercise_sheet_7.r
@@ -50,7 +50,7 @@ data <- lexdec
 ## b) Run a simple regression, just including Frequency as predictor and RT as the dependent variable
 ##  Store it in lm1
 
-lm1 <- lm(Frequency ~ RT, data=data)
+lm1 <- lm(RT ~ Frequency, data=data)
 
 ## c) Report and explain the effect of Frequency
 
@@ -69,22 +69,39 @@ ggplot(data, aes(x=RT, y=Frequency)) +
   geom_smooth(method=lm, se=FALSE)
 
 ## e) Next, fit a model including Frequency and PrevType as predictors, store it in lm2
+lm2 <- lm(RT ~ Frequency+PrevType, data=data)
+
+summary(lm2)
 
 ## f) Report and explain the effects of Frequency and PrevType.
 
+## According to the p-value of the lm summary, we see that it is less than 0.05,
+## which means that the null hypothesis can be rejected - i.e. there is a significant
+## relationship between the frequency of the word and the reaction time. The same can be 
+## said of PrevType.
+
 ##  Next we want to plot a model where both predictors are shown. For those we first store the predicted values
 ## of our model:
- data$RT_pred = fitted(lm2)
+data$RT_pred = fitted(lm2)
 
 ## g) Now, plot the original data (RT by Frequency with different colors for PrevType), but use the 
 ## fitted values (RT_pred) inside geom_smooth() or geom_line(), otherwise, it will display regression lines 
 ## assuming an interaction
 ## The resulting plot should show the data points in different colors and two parallel regression lines.
 
+ggplot(data, aes(x=RT, y=Frequency, color=PrevType)) +
+  geom_point() +
+  geom_smooth(method=lm, se=FALSE, data=data)
+
 ## h) Run a regression model that includes also the interaction between Frequency and PrevType and store it
 ##  as lm3
 
+lm3 <- lm(RT ~ Frequency*PrevType, data=data)
+
 ## i) Plot the results of the model! (This time no need to specify the pred data set)
+
+plot(lm3)
+
 
 ## j) Report the results of lm3 and interpret with the help of the graph in i)
 
